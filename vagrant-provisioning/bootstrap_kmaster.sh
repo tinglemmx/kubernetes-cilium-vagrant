1c83072dec57e1e0142fc35370275b410b0a9864
Jiaqi Hu <tingle.mmx@hotmail.com>
2022-08-02 00:53:15 +0800

    change founction

--- vagrant-provisioning/bootstrap_kmaster.sh
+++ vagrant-provisioning/bootstrap_kmaster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "[TASK 1] Pull required containers"
-kubeadm config images pull >/dev/null 2>&1
+#echo "[TASK 1] Pull required containers"
+#kubeadm config images pull >/dev/null 2>&1
 
 echo "[TASK 2] Initialize Kubernetes Cluster"
 kubeadm init --apiserver-advertise-address=198.18.10.100 --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log 2>/dev/null
